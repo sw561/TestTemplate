8c9c9fa20a51970006a94677caa70dab8ba384c7
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2014-11-30 21:38:38 +0000

    Change compiled from global variable to class variable

--- program.py
+++ program.py
@@ -7,13 +7,12 @@ from os import system,path
 # This should also check whether the program has already been run with that input file.
 # If it has, it should recover the previously produced data.
 
-compiled = False
-
 class Program:
+	# compiled has one value for all class instances
+	compiled = False
 	def __init__(self,folder,d):
 		self.folder = folder
 		self.d = d
-		global compiled
 
 		# Create folder if necessary
 		if not path.isdir(folder):
@@ -28,9 +27,9 @@ class Program:
 			self.create_input()
 
 		if not path.exists(self.out_name):
-			if not compiled:
+			if not Program.compiled:
 				system("make")
-				compiled = True
+				Program.compiled = True
 			self.run()
 	
 	def run(self):

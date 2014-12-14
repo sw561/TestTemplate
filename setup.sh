b8ef8ba3869e4a6d3c13b4dcd0d29b143512dea7
wilkinson <simon_wilkinson92@hotmail.com>
2014-12-14 00:54:09 +0000

    setup script for starting new projects

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ln -s ~/Documents/TestTemplate/test.h .
+ln -s ~/Documents/TestTemplate/test.cpp .
+ln -s ~/Documents/TestTemplate/program.py .
+ln -s ~/Documents/TestTemplate/parameter* .
+ln -s ~/Documents/TestTemplate/generate.py .
+
+cp ~/Documents/TestTemplate/debug.h .
+cp ~/Documents/TestTemplate/Makefile .
+cp ~/Documents/TestTemplate/script.py .
+cp ~/Documents/TestTemplate/test.py .
+cp ~/Documents/TestTemplate/param_list.txt .
+cp ~/Documents/TestTemplate/main.cpp .
